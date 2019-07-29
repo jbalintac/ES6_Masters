b5def051043269278da0c23c27284f6119f08704
Jordan Balintac <JordanBa@magenic.com>
2019-07-29 14:27:18 +0800

    Exercise2

--- Exercise2/Exercise2.js
+++ Exercise2/Exercise2.js
@@ -12,14 +12,34 @@ Acceptance Criteria
 
     The winners are: 
 
+3. For tied scores (optional handler), the number of tied contestants will appear as:
+
+    The winners are: 
+
     (1st) Foo scored 9 out of 10
     (2nd) Bar scored 7 out of 10
     (3rd) Baz scored 6 out of 10
 
-3. For tied scores (optional handler), the number of tied contestants will appear as:
-
-    (2nd) 3 scored 8 out of 10
-
 4. Use destructuring when retrieving the top 3 winners
 
 */
+
+class ContestScore
+{
+    constructor(name, score) {
+        this.name = name;
+        this.score = score;
+    };
+}
+
+let result = getResult([new ContestScore('Bar', 7),new ContestScore('Baz', 6),new ContestScore('Foo', 9)])
+console.log(result);
+
+
+function getResult(input) {
+
+  let result = 'The winners are: \n';
+
+  return result + input.sort((a, b) => b.score - a.score).map(({name, score}, index) => 
+  `${index + 1} ${name} scored  ${score} out of 10`).join('\n');
+}
