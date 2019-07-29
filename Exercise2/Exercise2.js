3f70264d35d7107d06d45866987599c4a7097436
Jordan Balintac <JordanBa@magenic.com>
2019-07-29 14:25:41 +0800

    Exercise2 semi done

---
+++ Exercise2/Exercise2.js
@@ -0,0 +1,25 @@
+/*
+
+A ranking tool was asked to be developed by a local talent contest organizer. The tool must:
+
+- Capture the names and scores of contestants rated 1 to 10 in the order of the contestants' performance
+- Sort the order of the scores from highest to lowest
+- Display the top 3 winners' names of the talent contest based on the first 3 highest scorers
+
+Acceptance Criteria
+1. Input is an array of scores, naming contestants optional
+2. Display the output as:
+
+    The winners are: 
+
+    (1st) Foo scored 9 out of 10
+    (2nd) Bar scored 7 out of 10
+    (3rd) Baz scored 6 out of 10
+
+3. For tied scores (optional handler), the number of tied contestants will appear as:
+
+    (2nd) 3 scored 8 out of 10
+
+4. Use destructuring when retrieving the top 3 winners
+
+*/
